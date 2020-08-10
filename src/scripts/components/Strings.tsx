7f3495613fca6535d47f1e8d327cb32368c2c464
Alan Price <alan.price@diverseinteractive.com>
2020-08-10 19:47:37 +0100

    add String styles

--- src/scripts/components/Strings.tsx
+++ src/scripts/components/Strings.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function Strings() {
     return (
-        <>
+        <div className="strings">
             {Array.apply(null, Array(6)).map((item, index) => (
-                <div key={index} className="string"></div>
+                <div key={index} className="strings__string"></div>
             ))}
-        </>
+        </div>
     );
 }

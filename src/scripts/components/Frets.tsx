c94fa831ad8874d48a38f0ae7dd77f07567c702c
Alan Price <alan.price@diverseinteractive.com>
2020-08-10 17:16:25 +0100

    Frets.tsx: create numberOfFrets prop

--- src/scripts/components/Frets.tsx
+++ src/scripts/components/Frets.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
 
+interface IFretsProps {
+    numberOfFrets: number;
+}
+
 const inlays: Number[] = [3, 5, 7, 9, 12, 15, 17, 19, 21, 24];
 
-export default function Frets(): JSX.Element {
+export default function Frets(props: IFretsProps): JSX.Element {
     return (
         <>
-            {Array.apply(null, Array(24)).map((item, index) => (
-                <div key={index} className="fretboard__fret">
-                    {inlays.includes(index + 1) && (
-                        <div
-                            className={`fretboard__fret-inlay ${
-                                index + 1 === 12 || index + 1 === 24
-                                    ? 'fretboard__fret-inlay--alternate'
-                                    : ''
-                            }`}
-                        ></div>
-                    )}
-                </div>
-            ))}
+            {Array.apply(null, Array(props.numberOfFrets)).map(
+                (item, index) => (
+                    <div key={index} className="fretboard__fret">
+                        {inlays.includes(index + 1) && (
+                            <div
+                                className={`fretboard__fret-inlay ${
+                                    index + 1 === 12 || index + 1 === 24
+                                        ? 'fretboard__fret-inlay--alternate'
+                                        : ''
+                                }`}
+                            ></div>
+                        )}
+                    </div>
+                ),
+            )}
         </>
     );
 }
